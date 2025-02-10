cee1561ecc2730f43357ea1a645b9c95aba25883
Alex Nørgaard <umbra@abstractumbra.dev>
2025-02-10 18:32:18 +0000

    add copy pf text button

---
+++ static/spells.js
@@ -0,0 +1,8 @@
+async function copyToClipboard(number) {
+    let entry = document.getElementById(`spell-${number}`);
+
+    await navigator.clipboard.writeText(`BLU Spell #${number} - Pref healer, but can switch\nHelpers welcome too! Sync or unsync works.`);
+    document.querySelectorAll('.spell-cb-button').forEach(btn => { btn.removeAttribute('disabled'); });
+
+    entry.setAttribute("disabled", "disabled");
+}
