e5e40e1f2725b0b260d0bc5635d065c398e91cd1
Alex Nørgaard <umbra@abstractumbra.dev>
2025-02-13 19:35:00 +0000

    fix blu blurb

--- static/spells.js
+++ static/spells.js
@@ -1,7 +1,7 @@
 async function copyToClipboard(number) {
     let entry = document.getElementById(`spell-${number}`);
 
-    await navigator.clipboard.writeText(`BLU Spell #${number} - Pref healer, but can switch\nHelpers welcome too! Sync or unsync works.`);
+    await navigator.clipboard.writeText(`BLU Spell #${number} - Pref healer, but can switch\nHelpers welcome too! Sync or helper boosted unsync works.`);
     document.querySelectorAll('.spell-cb-button').forEach(btn => { btn.removeAttribute('disabled'); });
 
     entry.setAttribute("disabled", "disabled");
