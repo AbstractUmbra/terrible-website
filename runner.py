ad109d6a9bbca8a4a2a962d80c98fbf6d95c2ea8
Alex Nørgaard <umbra@abstractumbra.dev>
2025-08-14 21:12:29 +0100

    fix runtime

--- runner.py
+++ runner.py
@@ -4,7 +4,7 @@ import uvicorn
 
 if __name__ == "__main__":
     host = "127.0.0.1" if os.getenv("SITE") else "0.0.0.0"  # noqa: S104 # acceptable
-    conf = uvicorn.Config("website:APP", host=host, port=8037, workers=5, proxy_headers=True, forwarded_allow_ips="*")
+    conf = uvicorn.Config("website:APP", host=host, port=8030, workers=5, proxy_headers=True, forwarded_allow_ips="*")
     server = uvicorn.Server(conf)
 
     server.run()
